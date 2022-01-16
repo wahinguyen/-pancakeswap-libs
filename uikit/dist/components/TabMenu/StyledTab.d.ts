cb3d890eeafa4fec1c74e9a51601b4ea8d707148
wahi <wahinguyen1999@gmail.com>
2022-01-16 14:05:29 +0700

    fix

--- uikit/dist/components/TabMenu/StyledTab.d.ts
+++
@@ -1,6 +0,0 @@
-interface StyledTabProps {
-    color: "card" | "textSubtle";
-    bgColor: "textSubtle" | "input";
-}
-declare const StyledTab: import("styled-components").StyledComponent<"button", import("styled-components").DefaultTheme, StyledTabProps, never>;
-export default StyledTab;
